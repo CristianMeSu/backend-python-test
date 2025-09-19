abdb9b5cce407d0f3d267d5da8ff874c4d8d0cdb
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-09-19 13:03:38 -0500

    changing keepalive to avoid https proxys

--- keepalive.py
+++ keepalive.py
@@ -2,12 +2,21 @@ import time
 import random
 import requests
 from datetime import datetime
+import os
 
+# URL a "pingear"
 URL = "https://backend-python-test.onrender.com/"
+
+# Ignorar cualquier proxy
+os.environ.pop('http_proxy', None)
+os.environ.pop('https_proxy', None)
+os.environ.pop('HTTP_PROXY', None)
+os.environ.pop('HTTPS_PROXY', None)
 
 def ping():
     try:
-        requests.get(URL, timeout=10)
+        # requests sin usar proxy
+        requests.get(URL, timeout=10, proxies={})
         print(f"{datetime.now()}: Ping enviado correctamente")
     except Exception as e:
         print(f"{datetime.now()}: Error en el ping:", e)
